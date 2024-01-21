cff9d059178d412771e6e8c2540cbc333351870a
Mrsirinam <120685147+Mrsirinam@users.noreply.github.com>
2024-01-21 23:34:37 +0300

    show cards

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import Navigo from "navigo";
 import { Header } from "./modules/Header/Header";
 import { Main } from "./modules/Main/Main";
 import { Footer } from "./modules/Footer/Footer";
+import { ProductList } from "./modules/ProductList/productList";
 
 const productSlider = () => {
   Promise.all([
@@ -42,17 +43,47 @@ const init = () => {
   const router = new Navigo("/", { linksSelector: 'a[href^="/"]' });
 
   router //отслеживаем адреса
-    .on("/", () => {
-      console.log("на главной");
-    })
-    .on("/category", (obj) => {
-      console.log("obj: ", obj);
-
-      console.log("category");
-    })
-    .on("/favorite", () => {
-      console.log("favorite");
-    })
+    .on(
+      "/",
+      () => {
+        new ProductList().mount(new Main().element, [1, 2, 3, 4]);
+      },
+      {
+        leave(done) {
+          console.log("leave");
+          done();
+        },
+        already() {
+          console.log("already");
+        },
+      }
+    )
+    .on(
+      "/category",
+      () => {
+        new ProductList().mount(
+          new Main().element,
+          [1, 2, 3, 4, 5, 6],
+          "Категория"
+        );
+      },
+      {
+        leave(done) {
+          done();
+        },
+      }
+    )
+    .on(
+      "/favorite",
+      () => {
+        new ProductList().mount(new Main().element, [1, 2, 3], "Избранное");
+      },
+      {
+        leave(done) {
+          done();
+        },
+      }
+    )
     .on("/search", () => {
       console.log("search");
     })
@@ -66,7 +97,15 @@ const init = () => {
       console.log("order");
     })
     .notFound(() => {
-      document.body.innerHTML = "<h2>Страница не найдена</h2>";
+      new Main().element.innerHTML = `
+			<h2>Страница не найдена</h2>  
+			<p>Через 5 секунд вы будете перенаправлены 
+			<a href="/">на главную страницу</a>
+			</p>`;
+
+      setTimeout(() => {
+        router.navigate("/");
+      }, 5000);
     });
 
   router.resolve(); //запускаем роутер
